668f3e620fed58e691061e9eb19f5706c5523500
yumauma20 <71yuma10@gmail.com>
2020-09-15 17:00:02 +0900

    react-router-dom導入

--- server/resources/js/components/App.js
+++ server/resources/js/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import GlobalNav from './GlobalNav';
+import Top from './Top';
+import About from './About';
 
 const App = () => {
     return(
-        <p>表示チェック</p>
+        <BrowserRouter>
+        <React.Fragment>
+            <GlobalNav />
+            <Switch>
+　　　　　　　　　　{/*完全一致のため、exactを付与*/}
+                <Route path="/" exact component={Top} /> 
+                <Route path="/about" component={About} />
+            </Switch>
+        </React.Fragment>
+        </BrowserRouter>
     )
 }
 
