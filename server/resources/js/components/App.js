7b26383abaa306cb971b0b54ed585ee489817f92
yumauma20 <71yuma10@gmail.com>
2020-09-15 15:26:16 +0900

    reactでのviewチェック

---
+++ server/resources/js/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return(
+        <p>表示チェック</p>
+    )
+}
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<App />, document.getElementById('app'));
+}
